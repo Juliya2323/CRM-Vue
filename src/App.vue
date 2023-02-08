7f51a40f9070402b5c4615787dc641af0aa7ea96
Juliya2323 <106690898+Juliya2323@users.noreply.github.com>
2023-02-08 11:24:26 +0300

    add pages

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,13 @@
 h1.title Pug
 </template>
 
+<script>
+import Analytics from './pages/Analytics.vue';
+import Dashboard from './pages/Dashboard.vue';
+
+
+</script>
+
 <style scoped lang="scss">
 
 </style>
