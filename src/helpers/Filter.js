ebfe1797be6ae414f5f4c411f5015fb2f904dc62
Juliya2323 <106690898+Juliya2323@users.noreply.github.com>
2023-02-17 13:45:49 +0300

    final ID filtration

--- src/helpers/Filter.js
+++ src/helpers/Filter.js
@@ -1,10 +1,20 @@
-export function filterId(items, flag) {
-    console.log(flag);
-    console.log(items);
+/* export function filterId(items, flag) {
     return {
-        'up': items.sort((a,b) => a.id + b.id),
-        'down': items.sort((a,b) => b.id - a.id),
-        /* 'down': items.sort((a,b) => a.id > b.id ? 1 : b.id > a.id ? -1 : 0),
-        'up': items.sort((a,b) => a.id < b.id ? 1 : b.id < a.id ? -1 : 0) */
+        'up': items.sort((a,b) => b.id - a.id),
+        'down': items.sort((a,b) => a.id - b.id),
+        'down': items.sort((a,b) => a.id > b.id ? 1 : b.id > a.id ? -1 : 0),
+        'up': items.sort((a,b) => a.id < b.id ? 1 : b.id < a.id ? -1 : 0) 
     }[flag]
+} */
+
+export function filterId(items, flag) {
+    
+    
+if(flag === 'up') {
+  items.sort((a, b) => b.id - a.id);
+}
+
+if(flag === 'down') {
+  items.sort((a, b) => a.id - b.id);
+}
 }
