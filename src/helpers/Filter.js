5afffb795a217ebad3a259d864e5bc6d4659be45
Juliya2323 <106690898+Juliya2323@users.noreply.github.com>
2023-02-17 13:27:43 +0300

    .

--- src/helpers/Filter.js
+++ src/helpers/Filter.js
@@ -2,8 +2,8 @@ export function filterId(items, flag) {
     console.log(flag);
     console.log(items);
     return {
-        'down': items.sort((a,b) => a.id + b.id),
-        'up': items.sort((a,b) => b.id - a.id),
+        'up': items.sort((a,b) => a.id + b.id),
+        'down': items.sort((a,b) => b.id - a.id),
         /* 'down': items.sort((a,b) => a.id > b.id ? 1 : b.id > a.id ? -1 : 0),
         'up': items.sort((a,b) => a.id < b.id ? 1 : b.id < a.id ? -1 : 0) */
     }[flag]
