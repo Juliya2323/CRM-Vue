723794b1e314ad7e4f6815041cccfc7f1a65e855
Juliya2323 <106690898+Juliya2323@users.noreply.github.com>
2023-03-02 18:06:37 +0300

    main

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import { router } from "./router/index.js";
-import Vue from 'vue';
 
 import "./assets/styles/index.css";
 import App from "./App.vue";
